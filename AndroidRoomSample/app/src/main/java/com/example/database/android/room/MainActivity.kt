9ed72b76f23430f1e9b4f770b313e8a738f48b9f
wcf <1533144693@qq.com>
2020-03-14 14:34:14 +0800

    add fun for livedata

--- AndroidRoomSample/app/src/main/java/com/example/database/android/room/MainActivity.kt
+++ AndroidRoomSample/app/src/main/java/com/example/database/android/room/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import com.example.database.android.room.global.App
 import com.example.database.android.room.global.App.Companion.userDao
@@ -17,9 +18,13 @@ class MainActivity : AppCompatActivity() {
         App.database = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database")
                 .allowMainThreadQueries().build()
         userDao = App.database.userDao()
-
         App.androidId = Settings.System.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
 
+        liveDataQuery()
+
+    }
+
+    private fun normalQuery() {
         Log.d(javaClass.name, "--------------------------------")
         val hhr = User("54654511", "hhr", "00000000000000000000000000000001")
         val wcf = User("316513113", "wcf", "00000000000000000000000000000002")
@@ -41,5 +46,34 @@ class MainActivity : AppCompatActivity() {
             Log.d(javaClass.name, "$it")
         }
         Log.d(javaClass.name, "--------------------------------")
+    }
+
+    private fun liveDataQuery() {
+        val users = userDao.autoUpdateLoadUsers()
+
+        users.observe(this, Observer {
+            Log.d(javaClass.name, "--------------------------------")
+            it.forEach { it1->
+                Log.d(javaClass.name, "$it1")
+            }
+        })
+
+        Thread {
+            val hhr = User("54654579", "hhr", "00000000000000000000000000000001")
+            val wcf = User("31653115", "wcf", "00000000000000000000000000000002")
+            val cht = User("24238423", "cht", "00000000000000000000000000000003")
+
+            Thread.sleep(3000)
+
+            userDao.insertUsers(hhr, wcf)
+
+            Thread.sleep(3000)
+
+            userDao.deleteUsers(wcf)
+
+            Thread.sleep(3000)
+
+            userDao.insertUsers(wcf, cht)
+        }.start()
     }
 }
--- AndroidRoomSample/app/src/main/java/com/example/database/android/room/model/User.kt
+++ AndroidRoomSample/app/src/main/java/com/example/database/android/room/model/User.kt
@@ -1,13 +1,20 @@
 package com.example.database.android.room.model
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 
-@Entity(tableName = "user")
+@Entity(
+    tableName = "user",
+    indices = [
+        Index(value = ["uid"], unique = true),
+        Index(value = ["username"], unique = true)
+    ]
+)
 class User(
-        @PrimaryKey val uid: String,
-        val username: String,
-        val token: String
+    @PrimaryKey val uid: String,
+    val username: String,
+    val token: String
 ) {
     override fun toString(): String {
         return "$uid, $username, $token"
@@ -30,4 +37,7 @@ interface UserDao {
 
     @Query("SELECT * FROM user WHERE username == :username")
     fun loadAllUsersByUsername(username: String): Array<User>
+
+    @Query("SELECT * FROM user")
+    fun autoUpdateLoadUsers(): LiveData<Array<User>>
 }
